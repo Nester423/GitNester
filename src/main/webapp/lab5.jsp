3a6449d4fa801ce683918e93f51ee753c92777d2
Jovi <Jovi@DESKTOP-526QHI7>
2021-06-20 04:02:29 +0300

    lab5 last END

--- src/main/webapp/lab5.jsp
+++ src/main/webapp/lab5.jsp
@@ -16,6 +16,13 @@
         <h1>Lab 5</h1>
         <h2>Варіант 12</h2>
         
+        <h2>Text:</h2>
+        <h2>The idea of adding sensors and intelligence to basic objects was discussed throughout the 1980s and 1990s (and there are arguably some much earlier ancestors), but apart from some early projects - including an internet-connected vending machine - progress was slow simply because the technology wasn't ready. Chips were too big and bulky and there was no way for objects to communicate effectively.\n" +
+                "Processors that were cheap and power-frugal enough to be all but disposable were needed before it finally became cost-effective to connect up billions of devices. The adoption of RFID tags - low-power chips that can communicate wirelessly - solved some of this issue, along with the increasing availability of broadband internet and cellular and wireless networking. The adoption of IPv6 - which, among other things, should provide enough IP addresses for every device the world (or indeed this galaxy) is ever likely to need - was also a necessary step for the IoT to scale. Kevin Ashton coined the phrase 'Internet of Things' in 1999, although it took at least another decade for the technology to catch up with the vision. \"The IoT integrates the interconnectedness of human culture - our 'things' - with the interconnectedness of our digital information system - 'the internet.' That's the IoT,\" Ashton told ZDNet.\n" +
+                "The IoT was initially most interesting to business and manufacturing, where its application is sometimes known as machine-to-machine (M2M), but the emphasis is now on filling our homes and offices with smart devices, transforming it into something that's relevant to almost everyone. Early suggestions for internet-connected devices included 'blogjects' (objects that blog and record data about themselves to the internet), ubiquitous computing (or 'ubicomp'), invisible computing, and pervasive computing. However, it was Internet of Things and IoT that stuck.
+</h2>
+        
+        
         <div>
             <p>Невідсортований масив Integer: <%=request.getAttribute("unsortedList")%></p>        
             <p>Відсортований масив задопомогою sortApproach2: <%=request.getAttribute("sortedListv2")%></p>  
--- src/test/java/knu/fit/ist/ta/Lab5Tests.java
+++ src/test/java/knu/fit/ist/ta/Lab5Tests.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package knu.fit.ist.ta;
+
+import knu.fit.ist.ta.lab4.StringProcessing;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 /**
  *
  * @author Jovi
  */
 public class Lab5Tests {
+    StringProcessing sp = new StringProcessing();
     
+    String text1 = "Internet of things is the coming together of internet and physical devices in a "
+            + "network of unlimited possibilities using microcontrollers, arduino and raspberry pi. "
+            + "IOT allows for physical devices to wirelessly communicate over networks which has led to a growing number of applications for iot devices. "
+            + "Nevon Projects brings you the list of most innovative IOT projects for students and engineers using micocontroller arduino raspberry pi etc.";
+    // of - 5; and - 3; devices - 3; for - 3; the - 2; physical - 2
+    // 61 words without v
+    // 15 words with 3 unique let
+     
+    String text2 = "Internet of things is the coming together of internet and physical devices in  in a";
+    // of - 2; in - 2; internet - 2 
+    // 14 words without v (15 words at all, exception-word: devices)
+    // 2 words with 3 unique let (the, and)
+    
+    String text3 = "";
+    
+    @Test
+        void test1() {  
+          assertEquals(sp.find6MostFrequentWords(text1), "of and devices for the physical ");       
+          assertEquals(sp.findAmountOfWordsWithoutV(text1), 61);
+          assertEquals(sp.findAmountOfWordsWith3UniqueLet(text1), "15");
+          assertEquals(sp.find4MostPopular3Sequences(text1), "[ica=4, evi=3, ice=3, dev=3]");
+        }
+        
+     @Test 
+        void test2() {    
+          assertEquals(sp.find6MostFrequentWords(text2), "of in Internet    ");       
+          assertEquals(sp.findAmountOfWordsWithoutV(text2), 14);
+          assertEquals(sp.findAmountOfWordsWith3UniqueLet(text2), "2");
+          assertEquals(sp.find4MostPopular3Sequences(text2), "[rne=2, ern=2, ter=2, nte=2]");
+        }
+        
+    @Test 
+        void test3() {    
+          assertEquals(sp.find6MostFrequentWords(text3), "      ");       
+          assertEquals(sp.findAmountOfWordsWithoutV(text3), 0);
+          assertEquals(sp.findAmountOfWordsWith3UniqueLet(text3), "0");
+          assertEquals(sp.find4MostPopular3Sequences(text3), "[null, null, null, null]");
+        }
+ 
 }
